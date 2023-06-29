9172e1c7cc08a187b8eb41f3349582332d3a39b4
AlAlon369 <alalon211090@gmail.com>
2023-06-29 20:00:39 +0300

    Шаьлонные строки, оператор конкатенации строк

---
+++ Operators/ConcatenateOperator.ts
@@ -0,0 +1,21 @@
+// Оператор для конкатенации строк
+
+//`Hello ` + `World`    Если в 1-ом операнде не будет пробела, то две стринги будут написаны слитно
+
+// Переменные в конкатенации строк
+
+const hello = `Hello`
+const world = `World`
+
+const greetings = hello + ` ` + world
+
+// Шаблонные строки (Template String Literals)
+
+const hello1 = `Hello1`
+const world1 = `World1`
+
+const greetings1 = `${hello1} ${world1}` // Пролем в данном случае это часть строки.
+
+
+
+
